9e30f55b6ecc8044c1144d2f308653485ce838f6
SovathChean <sovathchean09gmail.com>
2020-05-07 17:22:47 +0700

    Test popup menu

--- app/src/main/java/com/example/aba/MainActivity.java
+++ app/src/main/java/com/example/aba/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.aba;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +16,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+   public void show_popup(View view)
+   {
+       ImageView img = findViewById(R.id.vertical_dot);
+       PopupMenu popup = new PopupMenu(this, img);
+       MenuInflater inflater = popup.getMenuInflater();
+       inflater.inflate(R.menu.popup_menu, popup.getMenu());
+       popup.show();
+   }
 }
